7c47ad4463e88335c58e9c30659c0325e69be232
kaylasam <kaylasamanthaa@gmail.com>
2021-05-10 14:02:29 -0500

    utilizing product-list.component in the app

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'pm-root',   //will be the directive used in html when refering to this component
   template: `
   <div><h1>{{pageTitle}}</h1>
-      <div>My First Component</div>
+      <pm-products></pm-products>
   </div>
   `
 })
--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({                 // defines class as an Angular module
   declarations: [           // declarations array defines what components belong to this module
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [                // defines the external modules we want to have available to 
     BrowserModule           // the components that belong to this module    
