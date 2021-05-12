5e3a05d3925f072480bece8df8b04b504fad6085
kaylasam <kaylasamanthaa@gmail.com>
2021-05-12 14:08:00 -0500

    move hardcoded products to a service

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
     selector: 'pm-products',
@@ -24,30 +25,10 @@ export class ProductListComponent implements OnInit{
     }
 
     filteredProducts: IProduct[] = [];
-
-    products: IProduct[] = [                             // "any" used when we dont know/care what the property type is
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2021",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "assets/images/leaf_rake.png"
-      },
-      {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-      }
-    ];  
+    products: IProduct[] = [];                             // "any" used when we dont know/care what the property type is 
     
+    constructor(private productService: ProductService){}
+
     performFilter(filterBy: string): IProduct[] {       //function takes in filter string and returns array of products
         filterBy = filterBy.toLocaleLowerCase();        //convert filter string to lowercase for easy comparison
         return this.products.filter((product: IProduct) => 
@@ -59,7 +40,8 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        this.listFilter = 'cart';                   //sets default value for listFilter
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
     }
     
     onRatingClicked(message: string): void {
