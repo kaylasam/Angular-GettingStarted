8f699e00513e28a78a96e3458b9fa01da6fdec35
kaylasam <kaylasamanthaa@gmail.com>
2021-05-12 12:48:54 -0500

    click on stars functionality

--- APM/src/app/shared/star.component.ts
+++ APM/src/app/shared/star.component.ts
@@ -7,10 +7,14 @@ import { Component, Input, OnChanges } from "@angular/core";
     styleUrls: ['./star.component.css']
 })
 export class StarComponent implements OnChanges{
-    @Input() rating: number = 0;        //add Input decorator to any property we want passed in 
+    @Input() rating: number = 0;        // add Input decorator to any property we want passed in 
     cropWidth: number = 75;
 
     ngOnChanges(): void {
         this.cropWidth = this.rating * 75/5;
+    }
+
+    onClick(): void {                   // called when user clicks on the stars
+        console.log(`The rating ${this.rating} was clicked!`)       // ` for template literal use
     }
 }
